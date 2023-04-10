e89486513b8dc8a4d4a58a4824a1b4fb9ebc6bf7
alex <alexstew6050@gmail.com>
2023-04-09 18:18:37 -0600

    profile page with static rendering

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,30 @@
+import { type User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+
+export const profileRouter = createTRPCRouter({
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
+
+      if (!user || !user.username) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Could not find user with that username",
+        });
+      }
+
+      return filterUserForClient(user);
+    }),
+});
